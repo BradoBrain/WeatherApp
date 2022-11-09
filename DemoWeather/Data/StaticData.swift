8247a2cca3a007711d0d8a586e9f04a49ade6b6c
Artem <97796448+BradoBrain@users.noreply.github.com>
2022-11-09 16:18:42 +0100

    Update StaticData.swift

--- DemoWeather/Data/StaticData.swift
+++ DemoWeather/Data/StaticData.swift
@@ -5,5 +5,5 @@ import Foundation
 /// Place to store user API key
 
 struct StaticData {
-    static let apiKey = "e34ddb0c9712b90931c36afb73c0459e"
+    static let apiKey = "Here should be your APIKey from https://openweathermap.org"
 }
